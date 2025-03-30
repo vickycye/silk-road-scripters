6a3e0b6b922ac8e7d4a22709488c4bf9ba8825b9
Vicky Ye <vickyye@uw.edu>
2025-03-30 10:31:02 -0700

    added content to main page -- shouldn't work yet

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
@@ -12,6 +13,33 @@ export default function Home() {
         width={500}
         height={500}
       />
+
+
+      {/* Vicky throws card stuff in here */}
+      <Head>
+        <title>Interactive Cards Gallery</title>
+        <meta name="description" content="Browse our collection of creative content!"/>
+        <link rel="icon" href="/favicon.ico"/>
+      </Head>
+
+      <main>
+        <div className="min-h-screen bg-gray-50">
+          <header className="bg-white shadow">
+            <div className="container mx-auto py-6 px-4">
+              <h1 className="text-2xl font-bold text-gray-800">content platform</h1>
+            </div>
+          </header>
+
+          <CardGrid />
+        </div>
+      </main>
+
+      <footer className="bg-gray-800 text-white py-4">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2025 Silk Road. All rights reserved.</p>
+        </div>
+      </footer>
+
     </div>
   );
 }
