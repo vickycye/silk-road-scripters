00c197e96fe04db75b7ebdb52a625a55bb44df22
Dishita <dishita@outlook.com>
2025-04-02 17:17:00 -0700

    adding searchbar file

---
+++ my-app/app/components /Searchbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Search } from 'lucide-react'
+
+export default function Navbar() {
+  return (
+        <div className='bg-green-300 rounded-md w-40 h-20'>
+            <Search size={20} color="#CCC9A1" strokeWidth={1}/>
+        </div>
+  );
+}
